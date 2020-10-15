a8b3fd7ef1bffc4329988af57aecb5412564a893
Rajalakshmi Priyanka Rajendran <rrajendran@uwm.com>
2020-10-15 12:36:41 -0400

    Updated .cs file

--- Test/UzoneReport.cs
+++ Test/UzoneReport.cs
@@ -7,7 +7,7 @@ using NUnit.Framework;
 using USFS.Library.TestAutomation;
 using USFS.Library.TestAutomation.Test;
 using USFS.Library.TestAutomation.Util;
-//using UzonePageObject;
+using UzonePageObject;
 
 namespace Test
 {
@@ -17,7 +17,7 @@ namespace Test
         private const string keyConnectionString = "ConnectionString";
         private const string keyUsername = "User";
         private const string keyPassword = "Password";
-        //private LandingPage landingPage;
+        private LoginPage loginPage;
 
 
         [OneTimeSetUp]
@@ -29,16 +29,37 @@ namespace Test
         [SetUp]
         public void SetUp()
         {
-            //landingPage = new LandingPage();
-
-
+            loginPage = new LoginPage();
             BasePage.StartBrowser(config[keyBrowserChoice], config[keyBrowserMode], config[keyResourceUsed]);
         }
 
         [Test]
-        public void YourtimeRegression()
+        public void UzoneReportGeneration()
         {
+            //Login
+            LoginUzoneTest();
+            //Connect DB and fetch records
+            //Goto seatingmap and take screenshot
+            //Create report in word format
+        }
 
+        public void LoginUzoneTest()
+        {
+            try
+            {
+                Log.Information("LoginUzoneTest has started");
+                string pageTitle = loginPage.LoginUzone(config[keyUrl], config[keyUsername], config[keyPassword]);
+                Log.Information("URL - " + config[keyUrl]);
+                ReportingUtil.test.Info("URL - " + config[keyUrl]);
+                StringAssert.AreEqualIgnoringCase("✔ YourTime", pageTitle, "Page title should contain '✔ YourTime'");
+                Log.Information("LoginUzoneTest has passed");
+                ReportingUtil.test.Pass("LoginUzoneTest has passed");
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "LoginUzoneTest Failed");
+                throw new Exception("LoginUzoneTest Failed", e);
+            }
         }
     }
 }

79bc0ce4da8f22e18113b88b1ab5f35909072ac8
Rajalakshmi Priyanka Rajendran <rrajendran@uwm.com>
2020-10-15 12:11:59 -0400

    Added json files

---
+++ Test/UzoneReport.cs
@@ -0,0 +1,50 @@
+﻿using System;
+    using System.Data;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.UI;
+    using Serilog;
+    using NUnit.Framework;
+    using USFS.Library.TestAutomation;
+    using USFS.Library.TestAutomation.Test;
+    using USFS.Library.TestAutomation.Util;
+//using UzonePageObject;
+
+namespace Test
+{
+    [TestFixture]
+    class UzoneReport : BaseTest
+    {
+    }
+
+
+    public class YourtimeIntegrationTest : BaseTest
+    {
+        private const string keyConnectionString = "ConnectionString";
+        private const string keyUsername = "User";
+        private const string keyPassword = "Password";
+        private const string keyPasswordLeader = "PasswordLeader";
+        private const string keyPasswordLeaderSenior = "PasswordLeaderSenior";
+
+        //private LandingPage landingPage;
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            //landingPage = new LandingPage();
+
+
+            BasePage.StartBrowser(config[keyBrowserChoice], config[keyBrowserMode], config[keyResourceUsed]);
+        }
+
+        [Test]
+        public void YourtimeRegression()
+        {
+
+        }
+    }
+}
